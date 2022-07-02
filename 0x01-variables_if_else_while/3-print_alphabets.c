111da4359963d7fca631d6d2bdc61cec8a89a86a
Matsheole <newwaysgneration@gmail.com>
2022-07-02 11:22:23 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+int main(void)
+{
+char a = 'a';
+char A = 'A';
+int i = 1;
+
+while (i <= 52)
+{
+if (i <= 26)
+{
+putchar(a);
+a++;
+}
+else
+{
+putchar(A);
+A++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
