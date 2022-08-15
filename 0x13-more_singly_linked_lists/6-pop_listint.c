68c605ecde4f91db2c5d85e48f52b3684689637e
Matsheole <newwaysgneration@gmail.com>
2022-08-15 03:24:57 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - a function that frees a listint_t list.
+ * @head: a pointer to the head of the listint_t.
+ *
+ * Return: nothing
+ */
+
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int i;
+
+if (!head || !*head)
+return (0);
+i = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (i);
+}
